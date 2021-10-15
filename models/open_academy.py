76b1f74decb0ebf06341c300392b8435592cae80
Alberto Medina <lamedina@unah.hn>
2021-10-14 23:16:19 -0600

    Views

---
+++ models/open_academy.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Course(models.Model):
+     _name = 'openacademy.course'
+     name = fields.Char(string="Title", required=True)    
+     description = fields.Text("Description")
+
+
+
+""" class open_academy(models.Model):
+    _name = 'open.academy'
+    _description = 'open__academy.open__academy'
+
+    name = fields.Char(string="Title", required=True)
+    description = fields.Text() """
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
